784aa70a1a14658f36608acde0a12006e8ae3336
Rodihat <74569318+Rodihat@users.noreply.github.com>
2020-11-20 16:16:55 +0000

    Added Lessons
    Used v-for to loop and display lessons

---
+++ script.js
@@ -0,0 +1,78 @@
+//Set up vue app
+let app = new Vue({
+    el: '#container',
+    data: {
+        lessons: [{
+            id: 1,
+            title: "Math",
+            location: "London",
+            spaces: 5,
+            price: 100,
+            image: "coursework/math.png"
+        },
+        {
+            id: 2,
+            title: "English",
+            location: "Oxford",
+            spaces: 6,
+            price: 70,
+            image: "coursework/english.png"
+        }, {
+            id: 3,
+            title: "Music",
+            location: "Liverpool",
+            spaces: 8,
+            price: 90,
+            image: "coursework/music.png"
+        }, {
+            id: 4,
+            title: "Math",
+            location: "Oxford",
+            spaces: 12,
+            price: 105,
+            image: "coursework/math.png"
+        }, {
+            id: 5,
+            title: "Theatre",
+            location: "Yorkshire",
+            spaces: 6,
+            price: 89,
+            image: "coursework/theatre.png"
+        }, {
+            id: 6,
+            title: "Chemistry",
+            location: "Birmingham",
+            spaces: 11,
+            price: 79,
+            image: "coursework/chem.png"
+        }, {
+            id: 7,
+            title: "Physics",
+            location: "London",
+            spaces: 15,
+            price: 150,
+            image: "coursework/physics.png"
+        }, {
+            id: 8,
+            title: "Economics",
+            location: "London",
+            spaces: 9,
+            price: 160,
+            image: "coursework/economics.png"
+        }, {
+            id: 9,
+            title: "Business Studies",
+            location: "London",
+            spaces: 7,
+            price: 75,
+            image: "coursework/business.png"
+        }, {
+            id: 10,
+            title: "Art",
+            location: "Kent",
+            spaces: 11,
+            price: 71,
+            image: "coursework/art.png"
+        },
+
+        ],
